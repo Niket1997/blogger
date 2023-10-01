00ab6b29722196385878fd817f9a0d66eec1d520
aniketmahangare <aniket.mahangare@jupiter.money>
2023-10-01 23:49:29 +0530

    added blog return

--- internal/blog/server.go
+++ internal/blog/server.go
@@ -2,6 +2,7 @@ package blog
 
 import (
 	"context"
+	"log"
 
 	"connectrpc.com/connect"
 	blogv1 "github.com/Niket1997/blogger/gen/blog/v1"
@@ -17,7 +18,17 @@ func (s *Server) Create(
 	ctx context.Context,
 	req *connect.Request[blogv1.CreateBlogRequest],
 ) (*connect.Response[blogv1.BlogResponse], error) {
-	panic("implement me")
+	log.Println("request received", req)
+	res := connect.NewResponse(&blogv1.BlogResponse{
+		BlogId:      "blog12345",
+		AuthorId:    "author12345",
+		Title:       "How to write gRPC clients in go",
+		Body:        "Some Body",
+		PublishedAt: 1,
+		UpdatedAt:   1,
+	})
+	res.Header().Set("Blog-Version", "v1")
+	return res, nil
 }
 
 func (s *Server) Get(
