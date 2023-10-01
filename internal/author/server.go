4914b42fc0f1161b6d9575506378c82e09f91306
aniketmahangare <aniket.mahangare@jupiter.money>
2023-10-01 23:24:55 +0530

    working API

--- internal/author/server.go
+++ internal/author/server.go
@@ -2,6 +2,7 @@ package author
 
 import (
 	"context"
+	"log"
 
 	"connectrpc.com/connect"
 	authorv1 "github.com/Niket1997/blogger/gen/author/v1"
@@ -16,14 +17,24 @@ func NewServer() *Server {
 	return &Server{}
 }
 
-func (s *Server) create(
+func (s *Server) Create(
 	ctx context.Context,
 	req *connect.Request[authorv1.CreateAuthorRequest],
 ) (*connect.Response[authorv1.AuthorResponse], error) {
-	panic("implement me")
+	log.Println("request received", req)
+	return &connect.Response[authorv1.AuthorResponse]{
+		Msg: &authorv1.AuthorResponse{
+			AuthorId:  "id12345",
+			Name:      "Niket",
+			EmailId:   "niket@gmail.com",
+			Bio:       "I'm awesome",
+			CreatedAt: 0,
+			UpdatedAt: 0,
+		},
+	}, nil
 }
 
-func (s *Server) get(
+func (s *Server) Get(
 	ctx context.Context,
 	req *connect.Request[authorv1.GetAuthorRequest],
 ) (*connect.Response[authorv1.AuthorResponse], error) {
--- internal/blog/server.go
+++ internal/blog/server.go
@@ -16,14 +16,14 @@ func NewServer() *Server {
 	return &Server{}
 }
 
-func (s *Server) create(
+func (s *Server) Create(
 	ctx context.Context,
 	req *connect.Request[blogv1.CreateBlogRequest],
 ) (*connect.Response[blogv1.BlogResponse], error) {
 	panic("implement me")
 }
 
-func (s *Server) get(
+func (s *Server) Get(
 	ctx context.Context,
 	req *connect.Request[blogv1.GetBlogRequest],
 ) (*connect.Response[blogv1.BlogResponse], error) {
